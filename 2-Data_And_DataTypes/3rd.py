ab4462c48331c0b554c5a72cfa59658bd9993ee3
Reza-a-nazari <rezanazari.phone@gmail.com>
2026-07-22 16:46:26 +0330

    V1 2

---
+++ 2-Data_And_DataTypes/3rd.py
@@ -0,0 +1,33 @@
+#Number Basre
+
+#Base 10
+
+a = 10
+
+# Base 2
+
+b = 0b1010 #21
+
+# Base 8
+
+c = 0o101 #65
+
+# Base 16
+
+d = 0x101 # 257
+
+
+###
+
+int('16',base=16)
+
+
+
+#base =0 for translating !
+
+
+format(10,'b') # 1010
+
+format(10,'b') # 0b1010 # show with litteral
+
+f"{10:b}"
---
+++ 2-Data_And_DataTypes/first.py
@@ -0,0 +1,21 @@
+# str(10) # out '10"
+
+print(dir(int))
+
+
+#What is Dounder ?
+# __name__
+#Like Double UnderLine
+# These are magic methods 
+# '__subclasshook__', 
+# '__truediv__', 
+# '__trunc__', '__xor__', 
+# 'as_integer_ratio', 
+# 'bit_count
+
+
+#in Ipython 
+# In [6]: (10).__str__()
+# Out[6]: '10'
+
+
